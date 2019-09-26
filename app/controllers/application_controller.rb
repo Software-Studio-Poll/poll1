cab10522a5033ec944923f2d2d1afd87d12f3027
Conrad <cleonik@tulane.edu>
2019-09-26 17:32:54 +0000

    made question views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def hello
+        render html: "¡Hola, mundo!"
+    end
+    
+    def goodbye
+        render html: "bye bye"
+    end
 end
