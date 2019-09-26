9ec553d0c5d5cbd2780bc975f8870bb311ad4fa9
Conrad <cleonik@tulane.edu>
2019-09-26 18:12:10 +0000

    seeding database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Create a main sample user.
+Question.create!(text:  "2+2",
+             answerA: "4",
+             answerB:              "fish")
+             
+Question.create!(text:  "2+3",
+             answerA: "4",
+             answerB:              "7")
