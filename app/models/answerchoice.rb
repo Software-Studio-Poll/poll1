7bc0911a16a8afb9d16582c1213dc751cadc1689
Conrad <cleonik@tulane.edu>
2019-11-12 00:44:38 +0000

    updated answerchoice model  to match validation0

--- app/models/answerchoice.rb
+++ app/models/answerchoice.rb
@@ -1,4 +1,10 @@
 class Answerchoice < ApplicationRecord
     belongs_to :question
-    validates :content, presence: true, length: { maximum: 500 }
+    #validates :text, presence: true, length: { in: 2..1200 }
+    validates :question_id, presence: true
+    validates :question_id, uniqueness: true
+    validates :content, length: { in: 1..1200 }
+    validates :content, :question_id, :tally, presence: true
+    
+    
 end
