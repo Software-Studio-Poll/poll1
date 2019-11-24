20b1c1cb043d1130c7397167aee31c21a6975360
Conrad <cleonik@tulane.edu>
2019-11-24 21:11:22 +0000

    add poll_id validation in question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ApplicationRecord
     belongs_to :poll
     has_many :answerchoices
     validates :text, presence: true, length: { in: 2..1200 }
+    validates :poll_id, presence: true
 end
