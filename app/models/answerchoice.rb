ab4d80711616ae4b05373418f83efc90ac118b87
Conrad <cleonik@tulane.edu>
2019-11-24 21:10:28 +0000

    remove unnecessary lines from answerchoice model

--- app/models/answerchoice.rb
+++ app/models/answerchoice.rb
@@ -1,9 +1,5 @@
 class Answerchoice < ApplicationRecord
     belongs_to :question
-    #validates :text, presence: true, length: { in: 2..1200 }
-    validates :question_id, presence: true
     validates :content, length: { in: 1..1200 }
     validates :content, :question_id, :tally, presence: true
-    
-    
 end
