b2acaf8dc023e20aefd932f0e8c2fd663fa1cc05
Conrad <cleonik@tulane.edu>
2019-12-01 00:34:06 +0000

    add allow_destroy and reject_if for nested attributes handling

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,5 +1,5 @@
 class Poll < ApplicationRecord
     has_many :questions, inverse_of: :poll
-    accepts_nested_attributes_for :questions
+    accepts_nested_attributes_for :questions, allow_destroy: true, reject_if: :all_blank
     belongs_to :user
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,5 +3,5 @@ class Question < ApplicationRecord
     has_many :answerchoices, inverse_of: :question
     validates :text, presence: true, length: { in: 2..1200 }
     validates :poll, presence: true
-    accepts_nested_attributes_for :answerchoices
+    accepts_nested_attributes_for :answerchoices, allow_destroy: true, reject_if: :all_blank
 end
