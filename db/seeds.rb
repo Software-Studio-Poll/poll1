103955c639f783f85f0129ad556f8d57cad12aa8
Thomas <troginsk@tulane.edu>
2019-12-03 01:47:05 +0000

    seeds updated for polls and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # Create a main sample user.
-20.times do |n|
-    quote = Faker::Movies::HarryPotter.quote
-    @q = Question.create!(text: "Who from Harry Potter said this: \n \"#{quote}\"")
+
+
+i = 0
+10.times do |m|
     
-    4.times do |o|
-        chara = Faker::Movies::HarryPotter.character
-        Answerchoice.create!(content: chara, question_id: @q.id, tally: 0)
+    fake_name = Faker::Name.name
+    fake_email = Faker::Internet.email
+    fake_pw = Faker::University.name
+    poll_title = fake_name + "'s Poll"
+    @harry = User.create!(name: fake_name, email: fake_email, permission:0, password: fake_pw, password_confirmation: fake_pw)
+    @harry_poll = Poll.create!(title: poll_title, user_id: @harry.id)
+    20.times do |n|
+        quote = Faker::Movies::HarryPotter.quote
+        @q = Question.create!(text: "Who from Harry Potter said this: \n \"#{quote}\"", poll_id: @harry_poll.id)
+        
+        4.times do |o|
+            chara = Faker::Movies::HarryPotter.character
+            Answerchoice.create!(content: chara, question_id: @q.id, tally: 0)
+        end
+        Answerchoice.create!(content: "None of the above.", question_id: @q.id, tally: 0)
+        i = i + 1
     end
-    Answerchoice.create!(content: "None of the above.", question_id: @q.id, tally: 0)
-    
 end
+
+
+
+
+
+
