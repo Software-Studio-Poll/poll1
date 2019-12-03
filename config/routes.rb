2fcd5c7e27d75acb060ba4f14cf4ecb442ac5387
Conrad <cleonik@tulane.edu>
2019-12-03 23:36:22 +0000

    make 'User' pages are not accessible via URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :polls
+  get 'users/:id', to: 'users#index'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
