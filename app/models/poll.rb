429d8156c3d6aaefd00fc291d2bdd21eab16a60e
Conrad <cleonik@tulane.edu>
2019-12-03 23:45:08 +0000

    improved form clarity and server-side validation

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,4 +2,5 @@ class Poll < ApplicationRecord
     has_many :questions, inverse_of: :poll
     accepts_nested_attributes_for :questions, allow_destroy: true, reject_if: :all_blank
     belongs_to :user
+    validates :title, presence: true, length: { in: 1..120 }
 end
