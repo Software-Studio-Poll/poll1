0a315e8df9008592367d26316e3379524822162d
Thomas Roginsky <troginsk@tulane.edu>
2019-12-11 03:35:33 +0000

    Make migration to remove tally from answerchoice table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_061224) do
+ActiveRecord::Schema.define(version: 2019_12_11_033027) do
 
   create_table "answerchoices", force: :cascade do |t|
     t.text "content"
     t.integer "question_id"
-    t.integer "tally"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
