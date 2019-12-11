d7a6eae4bd7b0f7a055442b0eefc76bf14b62877
Thomas Roginsky <troginsk@tulane.edu>
2019-12-11 06:07:23 +0000

    Remove user show route. (forgot)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :userchoices
   resources :polls
-  get 'users/:id', to: 'users#index'
+  #get 'users/:id', to: 'users#index'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
