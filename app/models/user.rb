18dd29abd6cc4d2b87a66af1e22b8c36bbd1ad70
Ben Johnson <bjohns24@tulane.edu>
2019-12-12 01:11:45 +0000

    user validation for email and name, as well as exapnding question text size in results view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :polls
     has_many :userchoices, inverse_of: :user
+    validates :name, presence: true, length: { in: 1..120 }
+    validates :email, presence: true, length: { in: 1..120 }
 end
